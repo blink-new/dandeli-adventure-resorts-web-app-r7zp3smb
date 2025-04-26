19be978ee56bfdc0262e02ec626e223cd29e047b
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-26 06:10:56 +0000

    Fix Server Startup Issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -10,4 +11,8 @@ if (!rootElement) {
   throw new Error("Failed to find the root element");
 }
 
-createRoot(rootElement).render(<App />);
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
